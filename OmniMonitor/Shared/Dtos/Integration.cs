837cd15e867eea0df24684d4ee13f50a2daf8ef7
IvanQuesoRallado <ivanrtx2001@gmail.com>
2025-09-13 16:57:37 -0300

    VAMOS DE NUEVO

---
+++ OmniMonitor/Shared/Dtos/Integration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace OmniMonitor.Shared.Dtos;
+    public class Integration
+{
+    [JsonPropertyName("name")]
+    public string? Name { get; set; }
+
+    [JsonPropertyName("externalId")]
+    public string? ExternalId { get; set; }
+}
