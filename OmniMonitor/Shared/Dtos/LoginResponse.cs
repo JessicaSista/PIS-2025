e6b1ae719cc60715ab4cae659828240ad0a8f0ad
maria.florencia.uriarte <mflorenciauriartetorres@gmail.com>
2025-09-15 19:10:31 -0300

    Login

---
+++ OmniMonitor/Shared/Dtos/LoginResponse.cs
@@ -0,0 +1,9 @@
+namespace OmniMonitor.Shared.Dtos
+{
+    public class LoginResponse
+    {
+        public bool Success { get; set; }
+        public string? Message { get; set; }
+        public User? User { get; set; }
+    }
+}
