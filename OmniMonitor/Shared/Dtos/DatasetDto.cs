ac0a6d68adf3a329d08ee1e889795f073499ecae
JessicaSista <sistajessica2@gmail.com>
2025-09-17 23:21:26 -0300

    features: gird, cartel, persistente en el navegador

--- OmniMonitor/Shared/Dtos/DatasetDto.cs
+++ OmniMonitor/Shared/Dtos/DatasetDto.cs
@@ -4,6 +4,8 @@ namespace OmniMonitor.Shared.Dtos
     {
         public int Id { get; set; }
         public string Nombre { get; set; } = string.Empty;
+        public string Descripcion { get; set; } = string.Empty; 
         public DateTime FechaCreacion { get; set; } = DateTime.Now;
+        public int RecordCount { get; set; } = 0;
     }
 }
