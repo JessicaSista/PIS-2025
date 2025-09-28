5f3683bce1fb541109e7c04dd6a1f6acefad1ad4
Bruno <elbrunoconde@gmail.com>
2025-09-28 19:38:56 -0300

    agregados eventos modulo um

---
+++ OmniMonitor/Shared/Dtos/EventType.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json.Serialization;
+
+namespace OmniMonitor.Shared.Dtos
+{
+    public class EventType
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; } = string.Empty; // obligatorio, minLength = 1
+
+        [JsonPropertyName("description")]
+        public string? Description { get; set; } // nullable
+
+        [JsonPropertyName("fields")]
+        public List<EventField>? Fields { get; set; }
+
+        [JsonPropertyName("icon")]
+        public string? Icon { get; set; }
+
+        [JsonPropertyName("requiresApproval")]
+        public bool RequiresApproval { get; set; }
+
+        [JsonPropertyName("ratingType")]
+        public string? RatingType { get; set; }
+
+        [JsonPropertyName("relevant")]
+        public bool Relevant { get; set; }
+
+        [JsonPropertyName("sendToExternal")]
+        public bool SendToExternal { get; set; }
+
+        [JsonPropertyName("showInList")]
+        public bool ShowInList { get; set; }
+    }
+}
---
+++ OmniMonitor/Shared/Dtos/News.cs
@@ -0,0 +1,54 @@
+﻿using OmniMonitor.Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace OmniMonitor.Shared.Dtos
+{
+    public class News
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("title")]
+        public string Title { get; set; } = string.Empty;
+
+        [JsonPropertyName("datePublished")]
+        public DateTime DatePublished { get; set; }
+
+
+        //No es el mismo tipo de usuario
+        //[JsonPropertyName("publisher")]
+        //public User Publisher { get; set; } = new User();
+
+        [JsonPropertyName("summary")]
+        public string Summary { get; set; } = string.Empty;
+
+        [JsonPropertyName("description")]
+        public string? Description { get; set; }
+
+        [JsonPropertyName("categories")]
+        public List<Category>? Categories { get; set; }
+
+        [JsonPropertyName("startDate")]
+        public DateTime? StartDate { get; set; }
+
+        [JsonPropertyName("endDate")]
+        public DateTime? EndDate { get; set; }
+
+        [JsonPropertyName("important")]
+        public bool Important { get; set; }
+
+        //[JsonPropertyName("images")]
+        //public List<string>? Images { get; set; }
+
+        //[JsonPropertyName("firstImage")]
+        //public string? FirstImage { get; set; }
+
+        //[JsonPropertyName("imagesRemoved")]
+        //public List<string>? ImagesRemoved { get; set; }
+
+        [JsonPropertyName("zone")]
+        public Zone Zone { get; set; } = new Zone();
+    }
+}
