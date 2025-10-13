7834150a062848617ae9effce63d486316b6940e
IvanQuesoRallado <ivanrtx2001@gmail.com>
2025-10-12 22:12:10 -0300

    COSA2

--- OmniMonitor/Shared/Dtos/JoinOperandDto.cs
+++ OmniMonitor/Shared/Dtos/JoinOperandDto.cs
@@ -3,5 +3,6 @@
     public ModuleType ModuleType { get; set; }
     public int DatasetId { get; set; }
     public EntityName EntityName { get; set; }
+    public string JoinPropertyType { get; set; } = "string";
     public string JoinPropertyName { get; set; } = string.Empty;
 }
