525771cfcbc6974431b547b5ecf192ebe179c4cf
Bruno <elbrunoconde@gmail.com>
2025-11-02 17:39:09 -0300

    fix1003

---
+++ OmniMonitor/Server/Migrations/20251102203821_cosopum.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OmniMonitor.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class cosopum : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_GrupoDatasets_DatasetsIM_id_dataset",
+                table: "GrupoDatasets");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GrupoDatasets_Datasets_id_dataset",
+                table: "GrupoDatasets",
+                column: "id_dataset",
+                principalTable: "Datasets",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_GrupoDatasets_Datasets_id_dataset",
+                table: "GrupoDatasets");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GrupoDatasets_DatasetsIM_id_dataset",
+                table: "GrupoDatasets",
+                column: "id_dataset",
+                principalTable: "DatasetsIM",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
