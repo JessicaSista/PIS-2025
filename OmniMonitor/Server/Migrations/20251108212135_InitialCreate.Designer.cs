e599b591a38fef81fcb4a9f2bf9b8a2768a160d8
brunocamiletti <brunocamiletti98@gmail.com>
2025-11-08 18:24:23 -0300

    redireccion dataset

--- OmniMonitor/Server/Migrations/20251108212135_InitialCreate.Designer.cs
+++ OmniMonitor/Server/Migrations/20251108212135_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using OmniMonitor.Server.Context;
 namespace OmniMonitor.Server.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20251107001121_InitialCreate")]
+    [Migration("20251108212135_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
