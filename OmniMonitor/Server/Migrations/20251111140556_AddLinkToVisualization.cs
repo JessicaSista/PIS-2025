20c3b7b030439031b199b6aba9a14a72fb15765b
soficamach0 <136126201+soficamach0@users.noreply.github.com>
2025-11-11 18:02:14 -0300

    LInk agregado
    Se agrega el link

---
+++ OmniMonitor/Server/Migrations/20251111140556_AddLinkToVisualization.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OmniMonitor.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLinkToVisualization : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "link",
+                table: "Visualizaciones",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "link",
+                table: "Visualizaciones");
+        }
+    }
+}
